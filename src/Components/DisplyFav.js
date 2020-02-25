b733e637dc565e11de89409cfd188375ecf88dac
RanenDev <github/RanenDev>
2020-02-25 16:53:43 +0300

    add child component

---
+++ src/Components/DisplyFav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import FavItem from './FavItem';
+
+//Second Create List Component
+export default class DisplyFav extends React.Component {
+  render() {
+
+//Create map function 
+    const FavAlbum = this.props.Fav.map((post,index) => {
+      //  console.log(List)
+      //Call Item component 
+      return <FavItem post={post} key={index} />
+     });
+
+     //Call the map function
+    return (
+      <div className="post-list">
+       {FavAlbum}
+      </div>
+    );
+  } 
+}
