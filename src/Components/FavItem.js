38691bb5f20b4067cd3c0c8f1c551e0f309e2118
RanenDev <github/RanenDev>
2020-02-25 16:54:42 +0300

    add child component

---
+++ src/Components/FavItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Second Create List Component
+export default class FavItem extends React.Component {
+  render() {
+
+//Create map function 
+
+
+     //Call the map function
+    return (
+      <div >
+        {this.props.post}
+      </div>
+    );
+  } 
+}
