5baed1e2f3309d330b8ec4cd5906b9805890f75b
RanenDev <github/RanenDev>
2020-02-26 16:05:27 +0300

    Updates

---
+++ precache-manifest.dbac649e4c551c721cc282b93ae1fa8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6505e1335acbfef66fdb4361a3259d",
+    "url": "/itunes-Search/index.html"
+  },
+  {
+    "revision": "271fc80ccb54397e7a94",
+    "url": "/itunes-Search/static/css/main.34848ea3.chunk.css"
+  },
+  {
+    "revision": "f5945b929db4a9e29716",
+    "url": "/itunes-Search/static/js/2.098d7293.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/itunes-Search/static/js/2.098d7293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271fc80ccb54397e7a94",
+    "url": "/itunes-Search/static/js/main.ebfef636.chunk.js"
+  },
+  {
+    "revision": "5d21522763207a80e92a",
+    "url": "/itunes-Search/static/js/runtime-main.b4a07c05.js"
+  }
+]);
